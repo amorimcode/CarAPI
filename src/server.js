7dd0e50cc1505d3400fbc60d155d3479119394a2
Bruno Amorim <bruno.amorim032@gmail.com>
2021-06-15 00:39:30 -0300

    Nodemon

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app')
 
 var PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`App listen on port : ${PORT}`);
+    console.log(`App listen on port: ${PORT}`);
 })
