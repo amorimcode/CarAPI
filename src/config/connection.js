11355babec1fdeea39bb1c4fedaf5e28ccc59ab3
Bruno Amorim <bruno.amorim032@gmail.com>
2021-06-15 12:17:56 -0300

    MongoDB connection

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 class Connection {
-    constructor() {
-        this.dataBaseConnectionMongoDB();
-    }
+  constructor() {
+    this.dataBaseConnectionMongoDB();
+  }
 
-    dataBaseConnectionMongoDB() {
-        this.mongoDBConnection = mongoose.connect("mongodb://localhost/nodejs", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        })
-            .then(() => {
-                console.log("Sucesso ao conectar no MongoDB")
-            })
-            .catch((error) => {
-                console.log(`Erro ao conectar no mongoDB: ${error}`)
-            })
-    }
+  dataBaseConnectionMongoDB(){
+    this.mongoDBConnection = mongoose.connect("mongodb://localhost/nodejs", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    })
+    .then(() => {
+      console.log("Conexão estabelicida com o MongoDB");
+    })
+    .catch((error) => {
+      console.log(`Erro ao estabelecer conexão com mongoDB: ${error}`)
+    })
+  }
 }
 
 module.exports = new Connection();
