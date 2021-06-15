c08bced412953e4e5dd67f3b36176c62f79d0a88
Bruno Amorim <bruno.amorim032@gmail.com>
2021-06-15 01:08:15 -0300

    Routes Post/Get test

--- src/app/Controllers/UserController.js
+++ src/app/Controllers/UserController.js
@@ -2,6 +2,16 @@ class UserController {
     index(req, res) {
         console.log(req.body)
     }
+
+    show(req, res) {
+
+        var users = ["Bruno", "Kamila"]
+        return res.status(200).json({
+            error: false,
+            users
+        })
+    }
+
 }
 
 module.exports = new UserController();
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const UserController = require('./app/Controllers/UserController')
 const routes = new Router();
 
 routes.post("/user", UserController.index);
+routes.get("/user", UserController.show);
 
 module.exports = routes;
