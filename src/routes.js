7e7c1b34f6469e70e9c1cbe0f5cfb03c53916674
Bruno Amorim <bruno.amorim032@gmail.com>
2021-06-16 15:17:31 -0300

    Token Authentication

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
+
+const AuthMiddleware = require('./app/Middlewares/AuthMiddleware')
 const LoginController = require('./app/Controllers/LoginController')
 const UserController = require('./app/Controllers/UserController')
 
 const routes = new Router();
 
 routes.post("/user", UserController.store);
-routes.get("/user", UserController.show);
+routes.get("/user", AuthMiddleware, UserController.show);
 routes.post("/login", LoginController.index);
 
 
