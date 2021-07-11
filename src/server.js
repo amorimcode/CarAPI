15ebd34491a2eeb34df4f501735d8e7a1380a771
Bruno Amorim <bruno.amorim032@gmail.com>
2021-07-11 14:09:35 -0300

    removing node_modules

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
     console.log(`App listen on port: ${PORT}`);
 })
